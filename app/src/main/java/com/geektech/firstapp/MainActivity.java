6ed6790a6b087ab29186f361029bc02aa069cc58
1Joma1 <thisisjoma@gmail.com>
2019-03-23 19:46:58 +0600

    First commit

---
+++ app/src/main/java/com/geektech/firstapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.geektech.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.btn1);
+        Button button2 = findViewById(R.id.btn2);
+        final TextView tv = findViewById(R.id.textView);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("hello");
+                Log.d("my log", "button pressed");
+            }
+        });
+    }
+
+}
